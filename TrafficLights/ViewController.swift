7cf36a50a6119c1df0b655712c1e9a1c38be6590
Alexander <alex.pismenskiy@gmail.com>
2020-10-14 23:29:15 +0300

    Add Button and circles

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redColor: UIView!
+    @IBOutlet weak var yellowColor: UIView!
+    @IBOutlet weak var greenColor: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColor.layer.cornerRadius = 75
+        redColor.alpha = 0.3
+        yellowColor.layer.cornerRadius = 75
+        yellowColor.alpha = 0.3
+        greenColor.layer.cornerRadius = 75
+        greenColor.alpha = 0.3
+        startButton.layer.cornerRadius = 10
     }
-
-
 }
+
 
